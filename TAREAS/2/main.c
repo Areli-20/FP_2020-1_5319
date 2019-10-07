7488516e04d82eb27a5f45bca5e9c1efb1249b63
Areli <areligs2000@gmail.com>
2019-10-07 10:12:15 -0500

    tareas

---
+++ TAREAS/2/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, char *argv[]){
+	int n;
+	n=atoi(argv[1]);
+	if(n%2==0){
+		printf("1");
+	}
+	else{
+		
+		printf("0\n");
+	}
+	
+	
+	
+	return 0;
+}
